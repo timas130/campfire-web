2226a3cb3ce44f40e9ccb257b4a8a5a72d260727
sitting33 <me@33rd.dev>
2021-10-25 17:55:14 +0300

    comment quotes, i guess

--- components/publication/Comment.js
+++ components/publication/Comment.js
@@ -6,6 +6,24 @@ import FormattedText from "../FormattedText";
 import Karma from "../Karma";
 import React from "react";
 import classNames from "classnames";
+import {limitText} from "../../lib/text-cover";
+
+function CommentQuote({jsonDB}) {
+  let text = jsonDB.quoteText;
+  if (jsonDB.quoteCreatorName) {
+    const otherName = jsonDB.quoteCreatorName + ":";
+    if (text.startsWith(otherName)) {
+      text = text.substring(otherName.length);
+    }
+  }
+
+  return <div className={classes.quote}>
+    <FormattedText text={
+      (jsonDB.quoteCreatorName ? `{90A4AE ${jsonDB.quoteCreatorName}:}` : "") +
+      limitText(text, 64, 128)
+    } />
+  </div>;
+}
 
 export default React.forwardRef(function Comment({ comment, bestComment = false, full = false }, ref) {
   const jsonDB = typeof comment.jsonDB === "string" ? JSON.parse(comment.jsonDB) : comment.jsonDB;
@@ -18,13 +36,13 @@ export default React.forwardRef(function Comment({ comment, bestComment = false,
       <time dateTime={moment(comment.dateCreate).toISOString()} className={classes.time}>
         {moment(comment.dateCreate).locale("ru").fromNow()}
       </time>
+      {comment.changed && <span className={classes.time}>(ред.)</span>}
       {full && <>&nbsp;<Link href={`/post/${comment.parentUnitId}`}>
         <a className={classes.openPost}>Открыть пост</a>
       </Link></>}
     </header>
     <div className={classes.content}>
-      {/* TODO: add actual quotes */}
-      {jsonDB.quoteCreatorName && <span className={classes.quote}>{jsonDB.quoteCreatorName}, </span>}
+      {jsonDB.quoteId !== 0 && <CommentQuote jsonDB={jsonDB} />}
       <FormattedText text={jsonDB.J_TEXT} />
     </div>
     <div className={classes.footer}>
