413908f442001f93e00f9b113cacc6be63d271d7
sitting33 <me@33rd.dev>
2021-11-03 12:22:03 +0300

    fix /api/auth/login with redir=false

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,6 +1,6 @@
 import md5 from "md5";
 import {sendRequestAuthenticated} from "../../../lib/server";
-import {requireArguments} from "../../../lib/api";
+import {requireArguments, sendErrorIfFromRemote} from "../../../lib/api";
 import {logout} from "./logout";
 
 export default async function authLogin(req, res) {
@@ -17,16 +17,27 @@ export default async function authLogin(req, res) {
     if (!resp.J_RESPONSE.account) {
       // problem?
       // noinspection ExceptionCaughtLocallyJS
-      throw {code: "ERROR_UNAUTHORIZED", cweb: true};
+      throw {
+        code: "ERROR_UNAUTHORIZED",
+        messageError: "Remote returned account=null",
+        params: [],
+        cweb: true,
+        response: resp.J_RESPONSE,
+      };
     }
 
-    if (req.body.redir.toLowerCase() !== "false") {
+    if (req.body.redir.toString().toLowerCase() !== "false") {
       res.redirect("/");
     } else {
       res.send(resp.J_RESPONSE);
     }
   } catch (e) {
     logout(req, res);
+    if ("redir" in req.body && req.body.redir.toString().toLowerCase() === "false") {
+      sendErrorIfFromRemote(res, e);
+      return;
+    }
+
     if (e.code === "ERROR_UNAUTHORIZED") res.redirect("/auth/login?error=unauthorized");
     else res.redirect("/auth/login?error=unknown");
   }
