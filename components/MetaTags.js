a3da0eec1a5d72f38c14a8d633f6d3a64068dd48
sitting33 <me@33rd.dev>
2022-03-13 15:57:52 +0300

    hide social poster when not explicitly specified

--- components/MetaTags.js
+++ components/MetaTags.js
@@ -1,4 +1,4 @@
-export default function MetaTags({title, url, description = "", image = "https://campfire.moe/social-poster.png"}) {
+export default function MetaTags({title, url, description = "", image = ""}) {
   return <>
     <meta name="title" content={title} />
     <meta name="description" content={description} />
@@ -7,13 +7,13 @@ export default function MetaTags({title, url, description = "", image = "https:/
     <meta property="og:type" content="website" />
     <meta property="og:url" content={url} />
     <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
-    <meta property="og:image" content={image} />
+    {description && <meta property="og:description" content={description} />}
+    {image && <meta property="og:image" content={image} />}
 
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content={url} />
     <meta property="twitter:title" content={title} />
-    <meta property="twitter:description" content={description} />
-    <meta property="twitter:image" content={image} />
+    {description && <meta property="twitter:description" content={description} />}
+    {image && <meta property="twitter:image" content={image} />}
   </>;
 }
