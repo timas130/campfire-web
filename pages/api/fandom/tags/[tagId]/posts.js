c31a9c7332e6831f41ff632a30c3a3e071426ecd
sitting33 <me@33rd.dev>
2022-06-26 13:42:18 +0300

    fix CAMPFIRE-WEB-3B and CAMPFIRE-WEB-3A

--- pages/api/fandom/tags/[tagId]/posts.js
+++ pages/api/fandom/tags/[tagId]/posts.js
@@ -4,7 +4,9 @@ import {sendErrorIfFromRemote} from "../../../../../lib/api";
 export async function fetchTagPosts(req, res, tagId, offset = 0) {
   return (await sendRequestAlwaysAuthenticated(
     req, res, "RPostGetAllByTag", {offset, tagId}
-  )).J_RESPONSE.units;
+  )).J_RESPONSE.units.filter(a => a.unitType === 9);
+  // see: https://campfire.moe/api/fandom/207/tags/55300/posts
+  // see: https://data.sit.sh/share/issue/b6e085571d814996a947ed904cb2fcbe/
 }
 
 export default async function tagPostsHandler(req, res) {
