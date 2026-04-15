6bba690a3862db0de8a6d382a547a2c329221929
sit <me@sit.sh>
2026-04-15 21:23:12 +0300

    refactor(api): fetch current user via GraphQL me + RAccountsGet

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -1,30 +1,32 @@
-import {sendRequestAuthenticated} from "../../../lib/server";
-import {sendErrorIfFromRemote} from "../../../lib/api";
+import {sendRequestAuthenticated, graphql, readTokens} from "../../../lib/server";
+import {sendError, sendErrorIfFromRemote} from "../../../lib/api";
 
 export async function fetchUserInfo(req, res) {
+  const {accessToken} = readTokens(req, res);
+  if (!accessToken) {
+    throw {code: "ERROR_UNAUTHORIZED", messageError: "Not logged in", params: [], cweb: true};
+  }
+
+  const {me} = await graphql(`query { me { id } }`, {}, accessToken);
+  if (!me) {
+    throw {code: "ERROR_UNAUTHORIZED", messageError: "No current user", params: [], cweb: true};
+  }
+
   const resp = (await sendRequestAuthenticated(
-    req, res, "RAccountsLoginSimple", {
-      tokenNotification: "",
-    },
+    req, res, "RAccountsGet", {accountId: Number(me.id)},
   )).J_RESPONSE;
+
   if (!resp.account) {
-    throw {
-      code: "ERROR_UNAUTHORIZED",
-      messageError: "Remote returned account=null",
-      params: [],
-      cweb: true,
-      response: resp,
-    };
+    throw {code: "ERROR_UNAUTHORIZED", messageError: "account missing", params: [], cweb: true};
   }
   return resp.account;
 }
 
 export default async function userInfoHandler(req, res) {
   try {
-    res.send({
-      ...(await fetchUserInfo(req, res)),
-    });
+    res.send({...(await fetchUserInfo(req, res))});
   } catch (e) {
+    if (e?.code === "ERROR_UNAUTHORIZED") return sendError(res, e, 401);
     sendErrorIfFromRemote(res, e);
   }
 }
